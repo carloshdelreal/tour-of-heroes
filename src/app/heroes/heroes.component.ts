446bba5c45ace1ba29e6e31a3e5e01d98638e4ca
Carlos Del Real <carloshdelreal@gmail.com>
2020-05-22 16:27:49 -0500

    display a list of heroes and show the details of the selected one

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -12,8 +13,10 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  
+  heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
